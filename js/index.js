81e6501bc529719999f061568ed3e8deb94a772c
Shahid Peerzade <shahidpeerzade4183@gmail.com>
2018-03-19 10:55:25 -0700

    Dashboard Designed

---
+++ js/index.js
@@ -0,0 +1,41 @@
+$("#overview").mouseover(function () {
+    $(this).attr('src', $(this).data("hover"));
+  }).mouseout(function () {
+    $(this).attr('src', $(this).data("src"));
+});
+
+$("#total_revenue").mouseover(function () {
+    $(this).attr('src', $(this).data("hover"));
+  }).mouseout(function () {
+    $(this).attr('src', $(this).data("src"));
+});
+
+$("#enterprise_value").mouseover(function () {
+    $(this).attr('src', $(this).data("hover"));
+  }).mouseout(function () {
+    $(this).attr('src', $(this).data("src"));
+});
+
+$("#investor_perspective").mouseover(function () {
+    $(this).attr('src', $(this).data("hover"));
+  }).mouseout(function () {
+    $(this).attr('src', $(this).data("src"));
+});
+
+$("#grow_revenue").mouseover(function () {
+    $(this).attr('src', $(this).data("hover"));
+  }).mouseout(function () {
+    $(this).attr('src', $(this).data("src"));
+});
+
+$("#increase_spread").mouseover(function () {
+    $(this).attr('src', $(this).data("hover"));
+  }).mouseout(function () {
+    $(this).attr('src', $(this).data("src"));
+});
+
+$("#single_scenario_modelling").mouseover(function () {
+    $(this).attr('src', $(this).data("hover"));
+  }).mouseout(function () {
+    $(this).attr('src', $(this).data("src"));
+});
